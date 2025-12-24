10d55438d43ab6b23136cb2c920dcd24a43bd246
retr0reg <retr0@retr0.blog>
2025-12-24 15:55:16 -0500

    Refactor main function to remove command-line argument handling and demonstrate buffer overflow vulnerability with a hardcoded long input string.

--- main.c
+++ main.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
+int main() {
     char buffer[10];
+    char input[100] = "This is a very long string that will overflow the buffer";
     
-    if (argc < 2) {
-        printf("Usage: %s <input_string>\n", argv[0]);
-        return 1;
-    }
-    
-    // Dangerous: buffer overflow - copying argv[1] into 10-char buffer
-    strcpy(buffer, argv[1]);
+    // Dangerous: buffer overflow - copying 56+ chars into 10-char buffer
+    strcpy(buffer, input);
     
     printf("Buffer contents: %s\n", buffer);
     return 0;
